0589c186fd087df91c1063b84b8d13eb65c2024b
ramkumar87777 <ramsuryasankararudv@gmail.com>
2021-11-11 12:37:50 +0530

    mohangit

--- src/org/gittest2/Test2.java
+++ src/org/gittest2/Test2.java
@@ -1,12 +1,15 @@
 package org.gittest2;
 
 public class Test2 {
-private void empNmae() {
+private void empName() {
 	System.out.println("Mohan");
 
 }
+private void empMob() {
+	System.out.println("7010962871");
+}
 public static void main(String[] args) {
 	Test2 t=new Test2();
-	t.empNmae();
+	t.empName();
 }
 }
