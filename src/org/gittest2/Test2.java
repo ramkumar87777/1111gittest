39465ba1df1dcfc24318925eab5ab4eacf9df241
ramkumar87777 <ramsuryasankararudv@gmail.com>
2021-11-11 11:29:20 +0530

    gittest2

---
+++ src/org/gittest2/Test2.java
@@ -0,0 +1,12 @@
+package org.gittest2;
+
+public class Test2 {
+private void empNmae() {
+	System.out.println("Mohan");
+
+}
+public static void main(String[] args) {
+	Test2 t=new Test2();
+	t.empNmae();
+}
+}
